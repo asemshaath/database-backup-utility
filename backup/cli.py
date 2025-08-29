2be5fefd3d9a4b8d068d01ed19bcc09cc8676b47
Asem Shaath <shaathasem@gmail.com>
2025-08-29 17:28:58 -0400

    cli: setting up initial cli commands

---
+++ backup/cli.py
@@ -0,0 +1,52 @@
+import argparse
+
+
+def main():
+    """
+    This is the main function that contains the primary logic of the script.
+    """
+    # Call other functions or perform operations here
+    parser = argparse.ArgumentParser(
+                        prog='db-backup',
+                        description='Database Backup Utility',
+                        epilog='')
+    
+    subparsers = parser.add_subparsers(dest="command")
+    # Example usage:
+    # db-backup backup --db-type postgres --db mydb
+    """"
+        db-backup backup \
+        --db-type postgres \
+        --host localhost --db mydb --user admin \
+        --cloud s3 \
+        --bucket my-db-backups \
+        --path prod/postgres \
+        --region us-east-1
+    """
+
+    # backup command
+    backup_parser = subparsers.add_parser('backup', help='Backup a database')
+
+    backup_parser.add_argument('--host')
+
+    
+    backup_parser.add_argument('--config', help='Path to config file')
+    
+    backup_parser.add_argument('-u', '--user', help='Database username') 
+    backup_parser.add_argument('-p', '--password', help='Database password')
+    backup_parser.add_argument('-d', '--db', help='Database name')
+    backup_parser.add_argument('--db-type', help='Type of the database (e.g., postgres, mysql)')
+    backup_parser.add_argument('--port', type=int, help='Database port number')
+
+    backup_parser.add_argument('--type', help='Cloud provider (e.g., s3, gcs, or local for local filesystem)')
+    backup_parser.add_argument('--bucket', help='Cloud storage bucket name')
+    backup_parser.add_argument('--path', help='Path in the cloud storage bucket to store the backup or local path if type is local')
+    backup_parser.add_argument('--region', help='Cloud storage region (if applicable)')
+
+    args = parser.parse_args()
+    print(args.command)
+    if args.command == 'backup':
+        print(f"Backing up database {args.db} of type {args.db_type} to {args.type} bucket {args.bucket} at path {args.path}")
+        # Here you would add the logic to perform the backup
+if __name__ == '__main__':
+    main()
