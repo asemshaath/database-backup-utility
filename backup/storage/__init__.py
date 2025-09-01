70304554bb79c27de0939df57f8dd59d8154eac6
Asem Shaath <shaathasem@gmail.com>
2025-09-01 12:54:23 -0400

    storage: minor fixes

--- backup/storage/__init__.py
+++ backup/storage/__init__.py
@@ -7,6 +7,6 @@ STRATEGIES = {
 
 def get_storage_strategy(storage_type: str):
     try:
-        return STRATEGIES[storage_type]
+        return STRATEGIES[storage_type]()
     except KeyError:
         raise ValueError(f"Unsupported storage type: {storage_type}")
