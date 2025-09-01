76f37ec04c62f5940e92363dca3a7e0ce4cb2910
Asem Shaath <shaathasem@gmail.com>
2025-09-01 08:43:08 -0400

    storage: added startegy alias

---
+++ backup/storage/__init__.py
@@ -0,0 +1,12 @@
+from .localstorage import LocalStorage
+
+STRATEGIES = {
+    "local": LocalStorage,
+    # "mysql": MySQLBackup,
+}
+
+def get_strategy(storage_type: str):
+    try:
+        return STRATEGIES[storage_type]
+    except KeyError:
+        raise ValueError(f"Unsupported storage type: {storage_type}")
