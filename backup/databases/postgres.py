5877ff52978196301aa925c71a8d9e6db1bad8d9
Asem Shaath <shaathasem@gmail.com>
2025-09-01 08:39:36 -0400

    databases: pg to store in temp file - timestamp in file name

--- backup/databases/postgres.py
+++ backup/databases/postgres.py
@@ -1,8 +1,9 @@
 from .base import BackupStrategy
 # import psycopg2
-import sys
 import subprocess
 import os
+import tempfile
+import time
 
 class PostgresBackup(BackupStrategy):
     def backup(self, config):
@@ -17,19 +18,27 @@ class PostgresBackup(BackupStrategy):
         if password:
             env["PGPASSWORD"] = password
         
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        fd, temp_file = tempfile.mkstemp(suffix=".sql", prefix=f"{dbname}_{timestamp}_")
+        os.close(fd)
+
+        print(f"Backup created: {temp_file}")
+
         if host and port and dbname and user:
             cmd = [
                 "pg_dump",
                 "-h", host,
                 "-p", port,
                 "-d", dbname,
-                "-U", user
+                "-U", user,
+                "-f", temp_file
             ]
+
             process = subprocess.Popen(cmd,env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            output, error = process.communicate()
+            _, error = process.communicate()
             if process.returncode != 0:
                 raise Exception(f"pg_dump failed: {error}")
-            return output
+            return tempfile
         else:
             raise ValueError("Missing required config parameters")
     def restore(self, config):
