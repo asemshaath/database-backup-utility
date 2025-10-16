0e7b65050531d657b373f9fe450748989c9721a8
Asem Shaath <shaathasem@gmail.com>
2025-10-16 00:58:58 -0400

    cli.py: restore cli added

--- backup/cli.py
+++ backup/cli.py
@@ -2,6 +2,7 @@ import argparse
 from databases import get_strategy
 from storage import get_storage_strategy
 import os
+import sys
 
 def main():
     """
@@ -26,35 +27,36 @@ def main():
         --region us-east-1
     """
 
-    # backup command
-    backup_parser = subparsers.add_parser('backup', help='Backup a database')
+    db_parser = subparsers.add_parser('backup', help='Backup a database')
+    db_parser = subparsers.add_parser('restore', help='Restore a database')
 
 
     
-    backup_parser.add_argument('--config', help='Path to config file')
+    db_parser.add_argument('--config', help='Path to config file')
     
-    backup_parser.add_argument('--db-host', help='Database host')
-    backup_parser.add_argument('--db-user', help='Database username') 
-    backup_parser.add_argument('--db-pass', help='Database password')
-    backup_parser.add_argument('--db-name', help='Database name')
-    backup_parser.add_argument('--db-type', help='Type of the database (e.g., postgres, mysql)')
-    backup_parser.add_argument('--db-port', type=int, help='Database port number')
+    db_parser.add_argument('--db-host', help='Database host')
+    db_parser.add_argument('--db-user', help='Database username') 
+    db_parser.add_argument('--db-pass', help='Database password')
+    db_parser.add_argument('--db-name', help='Database name')
+    db_parser.add_argument('--db-type', help='Type of the database (e.g., postgres, mysql)')
+    db_parser.add_argument('--db-port', type=int, help='Database port number')
 
-    backup_parser.add_argument('--storage', help='Cloud provider (e.g., s3, gcs, or local for local filesystem)')
-    backup_parser.add_argument('--bucket', help='Cloud storage bucket name')
-    backup_parser.add_argument('--path', help='Path in the cloud storage bucket to store the backup or local path if type is local (e.g., /local/path/)')
-    backup_parser.add_argument('--region', help='Cloud storage region (if applicable)')
-    backup_parser.add_argument('--credentials', help='Path to cloud provider credentials file (if applicable)')
-    backup_parser.add_argument('--project', help='Project ID for Google Cloud Storage (optional)')
+    db_parser.add_argument('--storage', help='Cloud provider (e.g., s3, gcs, or local for local filesystem)')
+    db_parser.add_argument('--bucket', help='Cloud storage bucket name')
+    db_parser.add_argument('--path', help='Path in the cloud storage bucket to store the backup or local path if type is local (e.g., /local/path/)')
+    db_parser.add_argument('--region', help='Cloud storage region (if applicable)')
+    db_parser.add_argument('--credentials', help='Path to cloud provider credentials file (if applicable)')
+    db_parser.add_argument('--project', help='Project ID for Google Cloud Storage (optional)')
 
+    db_parser.add_argument('--backup-file', help='Path to the backup file for restoration (if applicable)')
 
     args = parser.parse_args()
     print(args.command)
-    print(args)
+
     if args.command == 'backup':
         print(f"Backing up database {args.db_name} of type {args.db_type} to {args.db_type} bucket {args.bucket} at path {args.path}")
-        db_backup = get_strategy(args.db_type)
-        storage_obj = get_storage_strategy(args.storage)
+        db = get_strategy(args.db_type)
+        storage = get_storage_strategy(args.storage)
 
         db_config = {
             "host": args.db_host,
@@ -64,7 +66,7 @@ def main():
             "password": args.db_pass
         }
 
-        db_file_path = db_backup.backup(config=db_config)
+        db_file_path = db.backup(config=db_config)
 
         storage_config = {
             "bucket": args.bucket,
@@ -75,11 +77,44 @@ def main():
             "project": args.project
         }
 
-        storage_obj.store(backup_path=db_file_path, config=storage_config)
+        storage.store(backup_path=db_file_path, config=storage_config)
 
         os.remove(db_file_path)
         print(f"Temporary backup file {db_file_path} removed.")
-        # Here you would add the logic to perform the backup
+    elif args.command == 'restore':
+        # Here you would add the logic to perform the restore
+        db = get_strategy(args.db_type)
+        storage = get_storage_strategy(args.storage)
         
+        storage_config = {
+            "bucket": args.bucket,
+            "path": args.path,
+            "region": args.region,
+            "type": args.storage,
+            "credentials": args.credentials,
+            "project": args.project
+        }
+
+        db_backup_path = storage.retrieve(backup_name= args.backup_file, config=storage_config)
+
+        db_config = {
+            "host": args.db_host,
+            "port": args.db_port,
+            "dbname": args.db_name,
+            "user": args.db_user,
+            "password": args.db_pass
+        }
+
+        db.restore(config={**db_config, "backup_file": db_backup_path})
+
+
+
+        os.remove(db_backup_path)
+        print(f"Temporary backup file {db_backup_path} removed.")
+
+    else:
+        parser.print_help()
+        sys.exit(1)
+
 if __name__ == '__main__':
     main()
